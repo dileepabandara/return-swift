bf3a4cb114557a7babc93f7e5030c53713886920
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-02-07 20:52:37 +0530

    Create 03_arithmetic_operators.swift

---
+++ 03_arithmetic_operators.swift
@@ -0,0 +1,19 @@
+import Swift
+
+var x = 0
+print("X: ", x)
+
+x = 4 + 2  // x is now 6
+print("X: ", x)
+
+x = 4 - 2  // x is now 2
+print("X: ", x)
+
+x = 4 * 2  // x is now 8
+print("X: ", x)
+
+x = 4 / 2  // x is now 2
+print("X: ", x)
+
+x = 4 % 2  // x is now 0
+print("X: ", x)
